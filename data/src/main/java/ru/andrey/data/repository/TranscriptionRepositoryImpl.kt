e4c85399c5dd8e7e5ee89bc614c101058e3462e1
AntipovAndrey <antipov11.11@gmail.com>
2019-01-08 16:58:33 +0300

    empty list instead of null if no map entry

--- data/src/main/java/ru/andrey/data/repository/TranscriptionRepositoryImpl.kt
+++ data/src/main/java/ru/andrey/data/repository/TranscriptionRepositoryImpl.kt
@@ -2,11 +2,19 @@ package ru.andrey.data.repository
 
 import ru.andrey.data.repository.dictionary.DictionaryService
 import ru.andrey.domain.model.Transcription
+import ru.andrey.domain.model.Variant.AMERICAN
+import ru.andrey.domain.model.Variant.BRITISH
 import ru.andrey.domain.repository.TranscriptionRepository
 
 class TranscriptionRepositoryImpl(private val dictionaryService: DictionaryService) : TranscriptionRepository {
 
     override fun getTranscription(word: String): Transcription {
-        return Transcription(word, dictionaryService.getTranscriptions(word))
+        val transcriptions = dictionaryService.getTranscriptions(word)
+        return Transcription(
+            word, mapOf(
+                AMERICAN to (transcriptions[AMERICAN] ?: emptyList()),
+                BRITISH to (transcriptions[BRITISH] ?: emptyList())
+            )
+        )
     }
 }
